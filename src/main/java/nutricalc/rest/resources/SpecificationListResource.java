80d872f6087a64ffaff0192b4b661805bd71d709
Tõnis Tuul <tonis.tuul@gmail.com>
2017-07-21 16:55:03 +0300

    Add lombok annotations to resources

--- src/main/java/nutricalc/rest/resources/SpecificationListResource.java
+++ src/main/java/nutricalc/rest/resources/SpecificationListResource.java
@@ -1,18 +1,14 @@
 package nutricalc.rest.resources;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.hateoas.ResourceSupport;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class SpecificationListResource extends ResourceSupport {
     private List<SpecificationResource> specifications = new ArrayList<>();
-
-    public List<SpecificationResource> getSpecifications() {
-        return specifications;
-    }
-
-    public void setSpecifications(List<SpecificationResource> specifications) {
-        this.specifications = specifications;
-    }
 }
--- src/main/java/nutricalc/rest/resources/SpecificationResource.java
+++ src/main/java/nutricalc/rest/resources/SpecificationResource.java
@@ -1,8 +1,12 @@
 package nutricalc.rest.resources;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import nutricalc.core.models.entities.Specification;
 import org.springframework.hateoas.ResourceSupport;
 
+@Data
+@EqualsAndHashCode(callSuper = true)
 public class SpecificationResource extends ResourceSupport {
     private String name;
     private String producer;
@@ -17,50 +21,10 @@ public class SpecificationResource extends ResourceSupport {
     private double salt;
     private String comment;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getProducer() {
-        return producer;
-    }
-
-    public void setProducer(String producer) {
-        this.producer = producer;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
     public void setTitle(String name, String producer) {
         this.name = name;
         this.producer = producer;
         this.title = name + " (" + producer + ")";
-    }
-
-    public double getEnergyKj() {
-        return energyKj;
-    }
-
-    public void setEnergyKj(double energyKj) {
-        this.energyKj = energyKj;
-    }
-
-    public double getEnergyKCal() {
-        return energyKCal;
-    }
-
-    public void setEnergyKCal(double energyKCal) {
-        this.energyKCal = energyKCal;
-    }
-
-    public double getFats() {
-        return fats;
     }
 
     public void setFats(double fats, double saturatedFats) {
@@ -68,53 +32,9 @@ public class SpecificationResource extends ResourceSupport {
         this.saturatedFats = saturatedFats;
     }
 
-    public double getSaturatedFats() {
-        return saturatedFats;
-    }
-
-    public void setSaturatedFats(double saturatedFats) {
-        this.saturatedFats = saturatedFats;
-    }
-
-    public double getCarbohydrates() {
-        return carbohydrates;
-    }
-
     public void setCarbohydrates(double carbohydrates, double sugars) {
         this.carbohydrates = carbohydrates;
         this.sugars = sugars;
-    }
-
-    public double getSugars() {
-        return sugars;
-    }
-
-    public void setSugars(double sugars) {
-        this.sugars = sugars;
-    }
-
-    public double getProtein() {
-        return protein;
-    }
-
-    public void setProtein(double protein) {
-        this.protein = protein;
-    }
-
-    public double getSalt() {
-        return salt;
-    }
-
-    public void setSalt(double salt) {
-        this.salt = salt;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
     }
 
     public Specification toSpecification() {
