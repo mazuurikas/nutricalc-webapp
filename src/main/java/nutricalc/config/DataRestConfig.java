2b4f0379646765e0d85a6e46e007b56f3a2cfd7c
Tõnis Tuul <tonis.tuul@gmail.com>
2017-10-02 22:01:24 +0300

    Add CORS support

---
+++ src/main/java/nutricalc/config/DataRestConfig.java
@@ -0,0 +1,15 @@
+package nutricalc.config;
+
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataRestConfig extends RepositoryRestConfigurerAdapter {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.getCorsRegistry().addMapping("/**")
+                .allowedOrigins("http://localhost:4200");
+    }
+}
